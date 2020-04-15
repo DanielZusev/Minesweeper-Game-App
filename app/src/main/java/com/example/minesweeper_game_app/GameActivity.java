8c263b9cf38112ac4415edf3718ac900faa3e0f7
Daniel <daniel.zusev@gmail.com>
2020-04-15 14:29:55 +0300

    Init

--- app/src/main/java/com/example/minesweeper_game_app/GameActivity.java
+++ app/src/main/java/com/example/minesweeper_game_app/GameActivity.java
@@ -1,9 +1,13 @@
 package com.example.minesweeper_game_app;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,29 +15,47 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.minesweeper_game_app.logic.CellAdapter;
 import com.example.minesweeper_game_app.logic.Game;
 
-import static com.example.minesweeper_game_app.R.id.gridView;
-
 public class GameActivity extends AppCompatActivity {
 
 
     Game mGame;
     GridView mGridView;
+    Button restartButton;
+    ImageView flagButton;
     CellAdapter mCellAdapter;
     TextView mPlayerTurnTextView;
-    int boardSize;
+    Chronometer timer;
 
+    int boardSize;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game);
-        boardSize = getIntent().getIntExtra("BOARD_SIZE", 0);
 
+
+        setContentView(R.layout.activity_game);
+
+        boardSize=getIntent().getIntExtra("BOARD_SIZE",0);
         mGame = new Game(boardSize);
 
-        mGridView = findViewById(gridView);
+        mGridView = findViewById(R.id.gridView);
+        restartButton =findViewById(R.id.restart_button);
+        flagButton =(ImageView)findViewById(R.id.flag_button);
+
         mGridView.setNumColumns(boardSize);
         mCellAdapter = new CellAdapter(this, mGame.getmBoard());
         mGridView.setAdapter(mCellAdapter);
+
+        timer = findViewById(R.id.chronometer);
+        timer.setFormat("Time: %s");
+        timer.start();
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mGame.getmBoard().CreateNewBoard(boardSize);
+                timer.setBase(SystemClock.elapsedRealtime());
+                timer.start();
+            }
+        });
 
         mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -44,6 +66,15 @@ public class GameActivity extends AppCompatActivity {
 
             }
         });
-
+        flagButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mGame.ToggleFlagged();
+                if(mGame.isFlagged())
+                    flagButton.setImageResource(R.drawable.flag_on);
+                else
+                    flagButton.setImageResource(R.drawable.flagged);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/minesweeper_game_app/logic/Game.java
+++ app/src/main/java/com/example/minesweeper_game_app/logic/Game.java
@@ -1,60 +1,49 @@
 package com.example.minesweeper_game_app.logic;
-
-
-
 public class Game {
-
     private Board mBoard;
+
+
     private boolean isFlagged;
     private int boardSize;
+
     public int cellsToWinCount;
 
-    public Game(int boardSize) {
-        setBoardSize(boardSize);
-        this.mBoard = new Board(this.boardSize);
-        this.isFlagged = false;
-    }
+    public Game(int boardSize){
+        this.mBoard=new Board(boardSize);
+        this.isFlagged=false;
 
-    public void OnClick(int pos) {
+
+    }
+    public void OnClick(int pos){
         //click on mine
-        if (!mBoard.OnClick(pos, isFlagged))
+        if(!mBoard.OnClick(pos,isFlagged))
             GameOver();
             //click on UNCOVERED cell
-        else if (mBoard.OnClick(pos, isFlagged)) {
+        else if(mBoard.OnClick(pos,isFlagged)){
             cellsToWinCount--;
+            if(cellsToWinCount==0)
+                Victory();
         }
-        if (cellsToWinCount == 0)
-            Victory();
-    }
 
-    private void ToggleFlagged() {
-        if (this.isFlagged)
-            this.isFlagged = false;
-        else this.isFlagged = true;
     }
-
+    public void ToggleFlagged(){
+        if(this.isFlagged)
+            this.isFlagged=false;
+        else this.isFlagged=true;
+    }
     private void Victory() {
         //TODO victory logic
     }
 
-    public void GameOver() {
+    public void GameOver(){
         //TODO GameOver logic
     }
 
     public Board getmBoard() {
         return this.mBoard;
     }
-
-    public int getBoardSize() {
-        return this.boardSize;
-    }
-
     public boolean isFlagged() {
         return isFlagged;
-    }
-
-    public void setBoardSize(int boardSize) {
-        this.boardSize = boardSize;
     }
 
 
